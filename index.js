67b7a25f066d926df56c022052eac524777ae29c
xdlomad <cheanmol@gmail.com>
2023-03-31 09:30:40 +0800

    login function

--- index.js
+++ index.js
@@ -39,6 +39,34 @@ function login(username, password) {
     }
 }
 
+//Register function
+function register(newusername, newpassword, newname, newemail) {
+
+    //verify if username is already in databse
+    let match = dbUsers.find(element => 
+        element.username == newusername
+    )
+    if (match) {
+        return ( "Error! username is already taken :D")
+    } else {
+        // add info into database
+        dbUsers.push({
+        username : newusername,
+        password : newpassword,
+        name : newname,
+        email : newemail
+        })
+
+        return ( "Registration successful! :D" )
+    }
+
+    
+}
+
+
+//call register function
+console.log(register ("nootnoot", "fish32", "pingu", "pingu@noot.com"))
+console.log(register ("nootnoot", "fish32", "pingu", "pingu@noot.com"))
 
 //call login function
-console.log (login("bob","ILikeToFixIt"))
+console.log (login("nootnoot","fish32"))
